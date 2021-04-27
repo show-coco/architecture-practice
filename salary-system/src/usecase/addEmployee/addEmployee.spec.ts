affc143280fc25f7bde5349d68f57efd17e5a0c3
Sho Sakai <show75@ezweb.ne.jp>
2021-04-28 01:06:55 +0900

    refactor: Carve out repeated code to beforeEach in addEmployee.spec.ts

--- salary-system/src/usecase/addEmployee/addEmployee.spec.ts
+++ salary-system/src/usecase/addEmployee/addEmployee.spec.ts
@@ -2,10 +2,17 @@ import EmployeeRepo from '../../repository/EmployeeRepo';
 import AddEmployee from './AddEmployee';
 
 describe('addEmployee', () => {
+  let empRepo: EmployeeRepo;
+  let addEmployee: AddEmployee;
+  beforeAll(() => {
+    empRepo = new EmployeeRepo();
+    addEmployee = new AddEmployee(empRepo);
+  });
+
   describe('When adding salaried employee', () => {
     it('should be stored', () => {
-      const empRepo = new EmployeeRepo();
-      const addEmployee = new AddEmployee(empRepo);
+      empRepo = new EmployeeRepo();
+      addEmployee = new AddEmployee(empRepo);
 
       addEmployee.execute(1, 'Ropital', 'Hiroshima', 'Salary', undefined, 30, undefined);
       const emp = empRepo.getEmployee(1);
@@ -16,18 +23,12 @@ describe('addEmployee', () => {
 
   describe('When adding salaried employee with wrong paramater', () => {
     test('should be throw an error', () => {
-      const empRepo = new EmployeeRepo();
-      const addEmployee = new AddEmployee(empRepo);
-
       expect(() => addEmployee.execute(1, 'Ropital', 'Hiroshima', 'Salary', 1000, undefined, undefined)).toThrow('Please enter salary for Salary type');
     });
   });
 
   describe('When adding houly employee', () => {
     it('should be stored', () => {
-      const empRepo = new EmployeeRepo();
-      const addEmployee = new AddEmployee(empRepo);
-
       addEmployee.execute(1, 'Ropital', 'Hiroshima', 'Hourly', 1000, undefined, undefined);
       const emp = empRepo.getEmployee(1);
 
@@ -37,18 +38,12 @@ describe('addEmployee', () => {
 
   describe('When adding houly employee with wrong paramater', () => {
     test('should be throw an error', () => {
-      const empRepo = new EmployeeRepo();
-      const addEmployee = new AddEmployee(empRepo);
-
       expect(() => addEmployee.execute(1, 'Ropital', 'Hiroshima', 'Hourly', undefined, undefined, undefined)).toThrow('Please enter hourlyRate for Hourly type');
     });
   });
 
   describe('When adding comission employee', () => {
     it('should be stored', () => {
-      const empRepo = new EmployeeRepo();
-      const addEmployee = new AddEmployee(empRepo);
-
       addEmployee.execute(1, 'Ropital', 'Hiroshima', 'Comission', undefined, 30, 10);
       const emp = empRepo.getEmployee(1);
 
@@ -58,9 +53,6 @@ describe('addEmployee', () => {
 
   describe('When adding comission employee with wrong paramater', () => {
     test('should be throw an error', () => {
-      const empRepo = new EmployeeRepo();
-      const addEmployee = new AddEmployee(empRepo);
-
       expect(() => addEmployee.execute(1, 'Ropital', 'Hiroshima', 'Comission', undefined, undefined, undefined)).toThrow('Please enter salary and comissionRate for Comission type');
     });
   });
